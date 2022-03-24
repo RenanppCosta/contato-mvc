9b3953ff6e4b7a81ab81b3eeb099ab4f97a7052f
Renanppcosta <renanppcosta@gmail.com>
2022-03-24 11:43:59 -0300

    Criado routers e controllers do contato

--- controllers/ContatosController.js
+++ controllers/ContatosController.js
@@ -1,6 +1,28 @@
+const req = require("express/lib/request")
+const res = require("express/lib/response")
+const uid = 1;
+
 module.exports = {
 
     listarContatos: (req, res)=>{
-        res.send("Olá... já já eu mando seu contatos")
+        let contatos = require(`../database/contatos_${uid}.json`);
+        res.send(contatos)
+    },
+    capturarContato:(req, res)=>{
+        let contatos = require(`../database/contatos_${uid}.json`);
+        let idDoContato = req.params.id;
+        let contato = contatos.find(
+            (c) =>{
+                return c.id == idDoContato;
+            }
+        );
+        
+        if(!contato){
+            res.send("Contato inexistente")
+        }else{
+            res.send(contato)
+        }
+
     }
 } 
+
